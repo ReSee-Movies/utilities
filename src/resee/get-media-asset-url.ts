19816e79057e4d2dbcb727df16deb4b9a7b996e5
Michael <mdeanjones@gmail.com>
2025-04-23 18:06:12 +0200

    feat: the UiImg component now accepts a Directus file descriptor as a source, and will fill in other props accordingly

--- src/resee/get-media-asset-url.ts
+++ src/resee/get-media-asset-url.ts
@@ -2,6 +2,14 @@ import { isObjectLike } from '../objects/is-object-like';
 import { isString } from '../strings/is-string';
 import { serializeQueryObject } from '../urls/query-serialization';
 
+export type DirectusFileDescriptor = {
+  id                 : string;
+  filename_download? : string;
+  description?       : string;
+  height?            : number;
+  width?             : number;
+};
+
 // See https://docs.directus.io/reference/files.html#custom-transformations
 export type AssetTransformConfig = {
   fit?                : 'cover' | 'contain' | 'inside' | 'outside';
@@ -25,14 +33,22 @@ const DefaultBaseUrl = 'https://api.reseemovies.com/assets/';
  * Generates a URL for fetching media assets from Directus.
  */
 export function getMediaAssetUrl(fileId: string, options?: ToURLOptions): string;
+export function getMediaAssetUrl(descriptor: DirectusFileDescriptor, options?: ToURLOptions): string;
 export function getMediaAssetUrl(fileId: string, friendlyName: string | null | undefined, options?: ToURLOptions): string;
 
-export function getMediaAssetUrl(fileId: string, nameOrOptions?: string | null | ToURLOptions, options?: ToURLOptions) {
-  let path: string = fileId;
+export function getMediaAssetUrl(
+  fileIdOrDescriptor: string | DirectusFileDescriptor,
+  nameOrOptions?: string | null | ToURLOptions,
+  options?: ToURLOptions,
+) {
+  let path: string = isString(fileIdOrDescriptor) ? fileIdOrDescriptor : fileIdOrDescriptor.id;
   let query: undefined | URLSearchParams = undefined;
 
   if (isString(nameOrOptions)) {
     path += `/${ nameOrOptions }`;
+  }
+  else if (isObjectLike(fileIdOrDescriptor) && isString(fileIdOrDescriptor.filename_download)) {
+    path += `/${ fileIdOrDescriptor.filename_download }`;
   }
 
   if (isObjectLike(nameOrOptions)) {
