0ad8f93eb2df2c8a38580e87a0b74348ebb06fd7
Michael <mdeanjones@gmail.com>
2025-04-23 09:22:16 +0200

    feat: tweaked the fromTmdbImageSize method to also support string integers that are not prefixed with a "w"

--- src/tmdb/from-tmdb-image-size.ts
+++ src/tmdb/from-tmdb-image-size.ts
@@ -1,16 +1,32 @@
+import { isNumber } from '../numbers/is-number';
+import { isString } from '../strings/is-string';
+
+export type FromTmdbImageSizeOptions = {
+  /* Causes the "original" image size to return undefined instead of POSITIVE_INFINITY. */
+  originalIsUndefined?: boolean;
+};
+
 /**
  * Takes a TMDB "size" value for movie poster, actor headshots, or whatever, and
- * returns the integer value portion. For "original" images, the POSITIVE_INFINITY
- * constant is returned (so that it is always largest), and for anything else
- * undefined is returned.
+ * returns the integer value portion. For the "original" image size, the POSITIVE_INFINITY
+ * constant is returned (so that it is always largest when sorted against a list of
+ * other size values) by default, and for anything else undefined is returned.
  */
-export function fromTmdbImageSize(value: string) {
-  if (value === 'original') {
-    return Number.POSITIVE_INFINITY;
+export function fromTmdbImageSize(value: unknown, options?: FromTmdbImageSizeOptions) {
+  if (isNumber(value)) {
+    return value;
   }
 
-  if (value.match(/w[0-9]+/)) {
-    return parseInt(value.substring(1));
+  if (isString(value)) {
+    if (value === 'original') {
+      return options?.originalIsUndefined ? undefined : Number.POSITIVE_INFINITY;
+    }
+
+    const match = value.match(/w?(\d+)/);
+
+    if (match) {
+      return parseInt(match[1]);
+    }
   }
 
   return undefined;
