bb501be7dc7424895f7c1cbb74dfee4cffa6ac78
Michael <mdeanjones@gmail.com>
2025-06-27 12:09:58 +0200

    fix: changed the behaviour of the `getStartOfDay` and `getEndOfDay` utilities so that, by default, they take into account the timezone offset so when output to ISO string they line up with values created on the server

--- src/dates/get-start-of-day.ts
+++ src/dates/get-start-of-day.ts
@@ -1,10 +1,33 @@
+export type GetStartOfDayOptions = {
+  /**
+   * A seed date, whose year-month-date values will be used.
+   */
+  date?: Date;
+
+  /**
+   * A boolean that indicates whether the created date's timezone
+   * offset (as all Javascript dates are local) will be added to
+   * the date instance. This has the effect of causing the resulting
+   * date to "be in UTC" without such a thing _really_ existing in JS.
+   * Most importantly, this allows dates created with this method to
+   * line up with server values (most good servers have their TZ set
+   * to GMT) for operations like `toUTCString()`.
+   *
+   * @default true
+   */
+  shiftEpoch?: boolean;
+};
+
 /**
- * Returns a Date instance set to the beginning of the current day. An
- * optional seed date can also be provided, whose year-month-date values
- * will be used.
+ * Returns a Date instance set to the beginning of the current day.
  */
-export function getStartOfDay(options?: { date?: Date }) {
+export function getStartOfDay(options?: GetStartOfDayOptions) {
   const mutable = options?.date ? new Date(options.date) : new Date();
   mutable.setHours(0, 0, 0, 0);
+
+  if (options?.shiftEpoch !== false) {
+    mutable.setMinutes(-1 * mutable.getTimezoneOffset());
+  }
+
   return mutable;
 }
