4390c69e21d7e0153e53b3aa394dd5cbebe1145b
Michael <mdeanjones@gmail.com>
2025-07-25 09:37:38 +0200

    feat: modified the isString utility method to accept the optional argument `withContent` which extends the type check for content within the string

--- src/strings/is-string.ts
+++ src/strings/is-string.ts
@@ -1,6 +1,19 @@
 /**
- * A simple type guard for checking whether a value is a string.
+ * Config options for the `isString` utility.
  */
-export function isString(value: unknown): value is string {
-  return typeof value === 'string';
+export type IsStringOptions = {
+  withContent?: boolean;
+};
+
+/**
+ * A simple type guard for checking whether a value is a string. Optionally,
+ * the test can be narrowed to include only strings with some sort of content
+ * other than whitespace.
+ */
+export function isString(value: unknown, options?: IsStringOptions): value is string {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  return options?.withContent === true ? value.trim().length > 0 : true;
 }
