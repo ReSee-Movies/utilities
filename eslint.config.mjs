7155c8a5bb1e47c04f71a4e8fad52402e44890e5
Michael <mdeanjones@gmail.com>
2025-08-25 11:28:02 +0200

    chore: update ESLint config for use in an independent package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,20 @@
-import ReseeLintConfig from '@resee-movies/eslint/eslint.config';
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
 
-export default ReseeLintConfig({ tsSupport: true });
+export default tslint.config(
+  eslint.configs.recommended,
+  tslint.configs.recommended,
+  tslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['tests/**', '!**/*.ts'],
+    extends: [tslint.configs.disableTypeChecked],
+  },
+);
